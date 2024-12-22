9bf5740be21e73e554cc58d4fd55bfacebdca1fd
kashifal <captainkashi4@gmail.com>
2024-12-23 02:46:32 +0500

    all set

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -2,7 +2,6 @@
 /// <reference types="@types/google.maps" />
 import React, { useState, useEffect, useRef } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
 import HeroImg from '../../../public/heroImg.png'
 
 const HeroSection = () => {
@@ -43,7 +42,7 @@ const HeroSection = () => {
       <div className="my-14 xl:min-h-[617px] text-white relative w-full bg-gradient-to-r from-[#6965FD] to-[#FBD9B9] px-4 sm:px-10 py-16 rounded-[15px]">
         <div className="xl:w-[40%] flex xl:text-left text-center xl:items-start flex-col items-center">
         <p className="font-[500] sm:text-[16px] text-sm">Know Your Home’s True Worth in Minutes</p>
-        <h1 className=" sm:text-[35px] font-bold text-[30px] md:text-[50px] xl:text-[67px] py-5 leading-tight">
+        <h1 className="font-bold sm:text-[35px] text-[30px] md:text-[50px] xl:text-[67px] py-5 leading-tight">
         Home Value Calculator
         </h1>
         <p className="font-[500] sm:text-[16px] text-sm">Accurate and Instant Home Valuation at Your Fingertips.</p>
@@ -57,12 +56,9 @@ const HeroSection = () => {
               className='bg-[#FEFEFF] w-full rounded-full pr-32 placeholder:truncate sm:pr-40 pl-4 py-3 text-black outline-none placeholder:text-sm sm:placeholder:text-[16px]' 
               placeholder='Enter Your Property Address' 
             />
-                <Link href='/loginSteps'>
-
             <button className="bg-[#6965FD] px-4 sm:px-6 py-2.5 absolute right-1.5 top-1/2 -translate-y-1/2 rounded-full text-[#FEFEFF] sm:text-[16px] text-sm sm:font-semibold">
               Get Valuation
             </button>
-            </Link>
         </div>
         </div>
         <div className="absolute xl:block hidden xl:w-auto w-1/2 -bottom-14 right-0">
